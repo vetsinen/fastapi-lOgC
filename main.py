01087b77ddc3c9220b41dfdb28c68c532d22b35a
vetsinen <vetsinen@gmail.com>
2022-11-03 09:10:22 +0200

    shows last added locations sorted

--- main.py
+++ main.py
@@ -1,32 +1,62 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
-from pydantic import BaseModel
-from repository import save_location
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+from repository import save_location, get_last_locations
+
+from pydantic import BaseModel, Field
+from bson import ObjectId
+from typing import Optional
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
 class Location(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
     title: str
     review: str
     long: float
     lat: float
 
 class Msg(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
     msg: str
 
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
 
 @app.get("/")
 async def root():
     return FileResponse('static/add-location.html')
 
 @app.post("/api")
-async def root(location: Location):
+async def location(location: Location):
     print(location)
     id = save_location(location)
-    return {"id": 42}
+    return {"status": 200}
+
+@app.get("/api")
+async def last_location():
+    return get_last_locations()
+
 
 @app.get("/path")
 async def demo_get():
--- repository.py
+++ repository.py
@@ -1,27 +1,47 @@
+import pymongo
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+from bson.json_util import dumps
+from bson.json_util import loads
+
 load_dotenv()
 client = MongoClient(os.environ['MONGOURL'])
+db = client.test
+collection = db.locations
 
 # {
 #     name: "Central Park",
 #     location: {type: "Point", coordinates: [-73.97, 40.77]},
 #     category: "Parks"
 # },
-
 def save_location(location):
-    db = client.test
-    collection = db.locations
     id = collection.insert_one({
         "title": location.title,
         "location": {
             "type": "Point",
             "coordinates":[location.long,  location.lat]
         },
-        "review": location.review
+        "review": location.review,
+        "creation": datetime.now(),
     }).inserted_id
     return id
+
+def get_last_locations():
+    #TODO: use pydantic serializer
+    cursor = collection.find().limit(3).sort([("creation", pymongo.DESCENDING)])
+    docs =  loads(dumps(cursor))
+
+    rez = []
+    for loc in docs:
+        print(type(loc), str(loc['_id']))
+        rez.append({
+            "id": str(loc['_id']),
+            "title": loc['title'],
+            "review": loc['review'][0:15]
+        })
+    return rez
 
 if __name__ == '__main__':
     db = client.test
